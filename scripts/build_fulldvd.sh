6fbe976d39ff8d9a58769664b591138fb42480bb
Aaron Brown <aaron@internet2.edu>
2014-07-15 14:41:54 -0400

    Add a script to build a chroot jail for building 32-bit install DVDs on 64-bit platforms

--- scripts/build_fulldvd.sh
+++ scripts/build_fulldvd.sh
@@ -6,11 +6,26 @@ if [ "$(id -u)" != 0 ]; then
 	exit -1
 fi
 
+CHROOT=
+BUILD_CHROOT=0
+
 ##############################
 # Process Arguments
 ##############################
 while [ $# -gt 0 ]; do
     case $1 in
+        --build-chroot)
+			BUILD_CHROOT=1
+			shift
+			;;
+
+
+        --chroot)
+			CHROOT=$2
+			shift
+			shift
+			;;
+
         --arch)
 			BUILD_ARCH=$2
 			if [ -z $BUILD_ARCH ]; then
@@ -24,7 +39,7 @@ while [ $# -gt 0 ]; do
 			shift
 			shift
 			;;
-		-*)
+	-*)
 			echo "Invalid arg: $1"
 			exit 1
 			;;
@@ -33,6 +48,12 @@ while [ $# -gt 0 ]; do
 			;;
     esac
 done
+
+ARCH=`arch`
+if [ "$BUILD_ARCH" != "$ARCH" -a -z "$CHROOT" -a -z "$BUILD_CHROOT" ]; then
+    echo "You need to build the DVD on a host with the same architecture (i386 or x86-64) as the DVD itself, or to specify a 'chroot' jail"
+    exit -1
+fi
 
 ##############################
 BUILD=pS-Performance_Toolkit
@@ -68,7 +89,6 @@ PATCHED_KICKSTART=`mktemp`
 echo "Applying $KICKSTART_PATCH to $KICKSTART_FILE."
 pushd $KICKSTARTS_DIRECTORY > /dev/null 2>&1
 
-# Set correct build architechture
 cp $KICKSTART_PATCH $TEMP_KICKSTART_PATCH
 sed -i "s/\[BUILD_ARCH\]/$BUILD_ARCH/g" $TEMP_KICKSTART_PATCH
 #uncomment arch specific lines
@@ -82,6 +102,41 @@ else
 fi
 popd > /dev/null 2>&1
 
-pushd $REVISOR_CONF_DIRECTORY
-revisor --cli --respin --model pspt-$BUILD_ARCH --product-version "$BUILD_VERSION" --iso-label "$BUILD_SHORT" --product-name "$BUILD" --kickstart $PATCHED_KICKSTART --kickstart-include --kickstart-default --install-dvd --debug 9 --config $REVISOR_CONF_DIRECTORY/revisor.conf --destination-directory `pwd`
+if [ "$BUILD_CHROOT" == "1" ]; then
+    if [ ! -x $SCRIPTS_DIRECTORY/build_revisor_chroot.sh ]; then
+        echo "The script to build a chroot is missing"
+        exit -1
+    fi
+
+    CHROOT=`mktemp -d`
+    $SCRIPTS_DIRECTORY/build_revisor_chroot.sh $CHROOT
+fi
+
+if [ -z "$CHROOT" ]; then
+    CHROOT="/"
+fi
+
+REVISOR_BUILD=/tmp/revisor_build
+CHROOT_REVISOR_BUILD=$CHROOT/$REVISOR_BUILD
+
+mkdir -p $CHROOT_REVISOR_BUILD
+
+cp $REVISOR_CONF_DIRECTORY/*conf $CHROOT_REVISOR_BUILD
+mv $PATCHED_KICKSTART $CHROOT_REVISOR_BUILD
+
+PATCHED_KICKSTART=`basename $PATCHED_KICKSTART`
+
+cat > $CHROOT_REVISOR_BUILD/build_dvd.sh <<EOF
+#!/bin/bash
+pushd $REVISOR_BUILD
+revisor --cli --respin --model pspt-$BUILD_ARCH --product-version "$BUILD_VERSION" --iso-label "$BUILD_SHORT" --product-name "$BUILD" --kickstart $PATCHED_KICKSTART --kickstart-include --kickstart-default --install-dvd --debug 9 --config revisor.conf --destination-directory \`pwd\`
 popd
+EOF
+
+chmod +x $CHROOT_REVISOR_BUILD/build_dvd.sh
+
+setarch $BUILD_ARCH chroot $CHROOT $REVISOR_BUILD/build_dvd.sh
+
+mv $CHROOT_REVISOR_BUILD/pspt-$BUILD_ARCH/iso/* .
+
+#rm -rfv $CHROOT_REVISOR_BUILD
---
+++ scripts/build_revisor_chroot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CHROOT_DIR=$1
+
+if [ "$(id -u)" != 0 ]; then
+	echo "Error: SuperUser privileges required to use this script."
+	exit -1
+fi
+
+if [ -z "$CHROOT_DIR" ]; then
+    echo "Need to specify the directory to use for building the chroot"
+    exit -1
+fi
+
+mkdir -p $CHROOT_DIR
+mkdir -p $CHROOT_DIR/var/lib/rpm
+rpm --rebuilddb --root=$CHROOT_DIR
+
+rpm -Uvh --nodeps --root=$CHROOT_DIR  http://mirror.centos.org/centos/6/os/i386/Packages/centos-release-6-5.el6.centos.11.1.i686.rpm
+
+setarch i686 yum --installroot=$CHROOT_DIR install -y rpm-build yum
+
+setarch i686 yum install -y --installroot=$CHROOT_DIR http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+setarch i686 yum install -y --installroot=$CHROOT_DIR http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/addons/revisor/revisor-cli-2.2-4.sl6_3.noarch.rpm http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/addons/revisor/sl-revisor-configs-1-6.3.4.noarch.rpm
+
+# Mount the virtual file systems in the chroot so we can build things properly
+mount --bind /proc $CHROOT_DIR/proc
+mount --bind /dev $CHROOT_DIR/dev
+
+# Allow us to resolve host names in the chroot jail
+cp /etc/resolv.conf $CHROOT_DIR/etc/
