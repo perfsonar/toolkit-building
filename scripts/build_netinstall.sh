5ed51f91a7c0bbab3e7c3800e5a46da11c8b5736
Andy Lake <andy@es.net>
2015-06-16 16:24:46 -0400

    Bumping version number

--- scripts/build_netinstall.sh
+++ scripts/build_netinstall.sh
@@ -40,7 +40,7 @@ done
 BUILD=pS-Performance_Toolkit
 BUILD_SHORT=pS-Toolkit
 BUILD_DATE=`date "+%Y-%m-%d"`
-BUILD_VERSION="3.4.2"
+BUILD_VERSION="3.5a1"
 BUILD_RELEASE=""
 BUILD_OS="CentOS6"
 BUILD_TYPE=NetInstall
